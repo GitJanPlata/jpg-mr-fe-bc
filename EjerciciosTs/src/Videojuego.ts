a76827d0f450085d32cc4cd90f767da9cb92a5be
GitJanPlata <96839905+GitJanPlata@users.noreply.github.com>
2023-06-28 17:07:02 +0200

    janCommit2

--- EjerciciosTs/src/Videojuego.ts
+++ EjerciciosTs/src/Videojuego.ts
@@ -1,45 +1,44 @@
 class Videojuego implements Entregable {
-    private titulo: string;
-    private horasEstimadas: number;
-    private entregado: boolean;
-    private genero: string;
-    private compañia: string;
+  private titulo: string;
+  private horasEstimadas: number;
+  private entregado: boolean;
+  private genero: string;
+  private compañia: string;
 
-    constructor(titulo: string, horasEstimadas: number, genero: string, compañia: string) {
+  constructor(titulo: string, horasEstimadas: number, genero: string, compañia: string) {
     this.titulo = titulo;
     this.horasEstimadas = horasEstimadas || 10;
     this.entregado = false;
     this.genero = genero || "";
     this.compañia = compañia || "";
+  }
 
-      entregar(): void {
-        this.entregado = true;
-      }
-    
-      devolver(): void {
-        this.entregado = false;
-      }
-    
-      isEntregado(): boolean {
-        return this.entregado;
-      }
-    
-      compareTo(a: Object): number {
-        if (a instanceof Videojuego) {
-          const videojuego = a as Videojuego;
-          if (this.horasEstimadas > videojuego.horasEstimadas) {
-            return 1;
-          } else if (this.horasEstimadas < videojuego.horasEstimadas) {
-            return -1;
-          } else {
-            return 0;
-          }
-        } else {
-          throw new Error("El objeto no es una instancia de Videojuego.");
-        }
-    }
+  entregar(): void {
+    this.entregado = true;
+  }
 
+  devolver(): void {
+    this.entregado = false;
+  }
+
+  isEntregado(): boolean {
+    return this.entregado;
+  }
+
+  compareTo(a: Object): number {
+    if (a instanceof Videojuego) {
+      const videojuego = a as Videojuego;
+      if (this.horasEstimadas > videojuego.horasEstimadas) {
+        return 1;
+      } else if (this.horasEstimadas < videojuego.horasEstimadas) {
+        return -1;
+      } else {
+        return 0;
+      }
+    } else {
+      throw new Error("El objeto no es una instancia de Videojuego.");
     }
+  }
 
 
     // Métodos get
@@ -81,4 +80,5 @@ class Videojuego implements Entregable {
     toString(): string {
     return `Título: ${this.titulo}\nHoras estimadas: ${this.horasEstimadas}\nEntregado: ${this.entregado}\nGénero: ${this.genero}\nCompañía: ${this.compañia}`;
     }
-}
+
+  }
